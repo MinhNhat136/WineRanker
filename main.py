63d40fa731795017ae43caab319df6c6a1291c3e
MinhNhat136 <nhatminhho13602@gmail.com>
2024-12-21 17:02:40 +0700

    add dataingestion pipeline

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-from src.MLOPS_datas_science import logger
+from src.data_science import logger
+from src.data_science.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 
-logger.info("Welcome to my first prj mlops")
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.initiate_data_ingestion()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
