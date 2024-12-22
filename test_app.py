6974404c1e229da1d2ae172372e937c14767f7e4
MinhNhat136 <nhatminhho13602@gmail.com>
2024-12-22 11:04:54 +0700

    test cicd.yaml

---
+++ test_app.py
@@ -0,0 +1,10 @@
+from app import app 
+
+def test_homePage():
+    pass
+
+def test_training(): 
+    response = app.test_client().get('/train')
+    assert response.status_code == 200
+    assert response.data == b"Training Successful!"
+
