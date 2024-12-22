999c376aa5b6c33fed35ec0b021c79d6f2b06892
MinhNhat136 <nhatminhho13602@gmail.com>
2024-12-22 11:15:49 +0700

    update test_app.py

--- test_app.py
+++ test_app.py
@@ -5,3 +5,10 @@ def test_training():
     assert response.status_code == 200
     assert response.data == b"Training Successful!"
 
+def test_home():
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+    assert b"<!DOCTYPE html>" in response.data
+
+def test_predict(): 
+    pass
